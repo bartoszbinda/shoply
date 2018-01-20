11c54705dedbd7bffad10303a4188d2681410b51
bartosz <batinex@gmail.com>
2018-01-20 16:40:41 +0100

    fixed issue with rspec ENCODING_FLAG not found

--- Gemfile
+++ Gemfile
@@ -41,11 +41,13 @@ end
 # gem 'unicorn'
 
 # Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
+ gem 'capistrano-rails', group: :development
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
   gem 'rspec-rails', '~> 3.7'
+  gem 'factory_bot'
+
 end
 
 group :development do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,9 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require "active_record/railtie"
+require "action_controller/railtie"
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -43,7 +46,17 @@ RSpec.configure do |config|
   # inherited by the metadata hash of host groups and examples, rather than
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  require 'spec_helper'
+  require 'rspec/rails'
+  # note: require 'devise' after require 'rspec/rails'
+  require 'devise'
 
+  RSpec.configure do |config|
+    # For Devise >= 4.1.0
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+    # Use the following instead if you are on Devise <= 4.1.1
+    # config.include Devise::TestHelpers, :type => :controller
+  end
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
